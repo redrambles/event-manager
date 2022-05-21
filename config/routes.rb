c735dc2d10fec31e9a5977f3510c8a5136e0db62
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2022-05-21 12:31:23 -0400

    Create event api

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
-Rails.application.routes.draw do
-  get 'site/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   get 'site/index'
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
 
 Rails.application.routes.draw do
   root to: 'site#index'
+
+  namespace :api do
+    resources :events, only: %i[index show create update destroy]
+  end
 end
